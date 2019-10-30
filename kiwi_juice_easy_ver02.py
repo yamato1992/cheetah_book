3f822bc7b0386f81aeb5ea196dcf344d5a9a8623
yamato mitsui <yamato.mitsui.orleans@gmail.com>
2019-10-30 12:22:21 +0900

    modify kiwi juice easy ver02

--- kiwi_juice_easy_ver02.py
+++ kiwi_juice_easy_ver02.py
@@ -1,11 +1,13 @@
+def the_pouring(capacities, bottoles, fromId, toId):
+    for i in range(0, len(fromId)):
+        total = bottoles[fromId[i]] + bottoles[toId[i]]
+        bottoles[toId[i]] = min(capacities[toId[i]], total)
+        bottoles[fromId[i]] = total - bottoles[toId[i]]
+    return bottoles
+
 capacities = [14, 35, 86, 58, 25, 62]
 bottoles = [6, 34, 27, 38, 9, 60]
 fromId = [1, 2, 4, 5, 3, 3, 1, 0]
 toId = [0, 1, 2, 4, 2, 5, 3, 1]
 
-for i in range(0, len(fromId)):
-    total = bottoles[fromId[i]] + bottoles[toId[i]]
-    bottoles[toId[i]] = min(capacities[toId[i]], total)
-    bottoles[fromId[i]] = total - bottoles[toId[i]]
-
-print(bottoles)
+print(the_pouring(capacities, bottoles, fromId, toId))
